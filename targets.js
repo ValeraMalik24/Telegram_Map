db7dda7b6654c0358e9304a91c7f5ba02cbe3198
ValeraMalik24 <91124790+ValeraMalik24@users.noreply.github.com>
2025-06-07 12:43:52 +0300

    Add files via upload

---
+++ targets.js
@@ -0,0 +1,18 @@
+let targets = [
+  {
+    id: "drone_1",
+    lat: 49.966,
+    lon: 36.353,
+    type: "shahed",
+    heading: 230,
+    radius: 8000
+  },
+  {
+    id: "drone_2",
+    lat: 49.820,
+    lon: 36.711,
+    type: "orlan",
+    heading: 180,
+    radius: 5000
+  }
+];
